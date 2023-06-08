e46487a112b6bf71c442d5cf725fa3502fe0197b
kajal774 <kajal@gmail.com>
2023-06-08 13:32:00 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Data from "./Data.json"
 
 function App() {
+  const[inputs,setinputs]=useState("")
+  console.log(inputs)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="text" placeholder="enter the name"  onChange={(e)=>setinputs(e.target.value)}>
+      </input>
+      <table>
+        <thead>
+          <tr>
+          <th>sl_no</th>
+          <th>first_name</th>
+          <th>last_name</th>
+          <th>email</th>
+          <th>gender</th>
+          <th>Phone_number</th>
+          </tr>
+        </thead>
+        <tbody>
+
+        
+      {
+        Data.filter((item)=>{
+          return inputs.toLowerCase()===""?item: item.first_name.toLowerCase().includes(inputs)
+
+        }).map((item)=>
+       <tr key={item.id}>
+          <td>{item.id}</td>
+          <td>{item.first_name}</td>
+          <td>{item.last_name}</td>
+          <td>{item.email}</td>
+          <td>{item.gender}</td>
+          <td>{item.Phone_number}</td>
+          </tr>
+        )  
+      }
+      </tbody>
+      </table>
+      
     </div>
   );
 }
